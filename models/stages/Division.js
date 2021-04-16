78745a0f8183a5c8fa42e64ee414930f4ecb544c
MohamedCharaabi <medch373@gmail.com>
2021-04-16 22:00:13 +0100

    fix schema stages

--- models/stages/Division.js
+++ models/stages/Division.js
@@ -6,22 +6,14 @@ export const divisionSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    dep_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Department',
-        // required: true,
-    },
+
     dep_name: {
         type: String,
         ref: 'Department',
         required: true,
         unique: true,
     },
-    dir_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Direction',
-        // required: true,
-    },
+
     dir_name: {
         type: String,
         ref: 'Direction',
