6a7140a4c2633ec6b69113ddf3d5e781934534c4
MohamedCharaabi <medch373@gmail.com>
2021-04-16 21:57:39 +0100

    fix schema stages

--- models/stages/Direction.js
+++ models/stages/Direction.js
@@ -7,22 +7,11 @@ export const directionSchema = new mongoose.Schema({
         required: true,
     },
 
-    dep: {
-
-        id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Department',
-            required: true,
-        },
-        name: {
-            type: String,
-            ref: 'Department',
-            required: true,
-            unique: true
-        },
+    dep_name: {
+        type: String,
+        ref: 'Department',
+        required: true,
     }
-
-
 
 
 
