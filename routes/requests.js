03480370481fb86cb21f8dcf1aafd188782e4549
MohamedCharaabi <medch373@gmail.com>
2021-05-02 23:38:35 +0100

    fix request fields

--- routes/requests.js
+++ routes/requests.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { getRequests, createRequest, requestStatus, updateRequest, deleteRequest, getFiltretRequests } from '../controllers/request.js'
+import { getRequests, createRequest, requestStatus, updateRequest, deleteRequest, getFiltretRequests, acceptRequest } from '../controllers/request.js'
 
 const router = express.Router();
 
@@ -9,6 +9,7 @@ router.get('/', getRequests);
 router.get('/filter/:rolePer/:Dep/:Dir/:Div/:Ser', getFiltretRequests);
 router.get('/:id', requestStatus);
 router.patch('/:id', updateRequest);
+router.patch('/:id', acceptRequest);
 router.delete('/:id', deleteRequest);
 
 
