53f0d87c74adfca06442d1b24dcf9c93a2600df7
MohamedCharaabi <medch373@gmail.com>
2021-06-16 12:38:49 +0100

    ahah

--- app.js
+++ app.js
@@ -28,10 +28,12 @@ const app = express();
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cookieParser())
-app.use(cors({
-    credentials: true,
-    origin: ['https://pfecims.web.app', 'http://localhost:3000', 'https://pfelanding.web.app', 'http://localhost:8000', 'http://localhost:4200', 'http://localhost:3001']  //3 react , 8 view, 42 angular
-}));
+// app.use(cors({
+//     credentials: true,
+//     origin: ['https://pfecims.web.app', 'http://localhost:3000', 'https://pfelanding.web.app', 'http://localhost:8000', 'http://localhost:4200', 'http://localhost:3001']  //3 react , 8 view, 42 angular
+// }));
+
+app.user(cors())
 
 app.use("/request", requestRoutes);
 app.use("/personnel", personnelRoutes);
